bff632c1b73380e987fa0b99aab8e84562d8bb55
Thomas ten Cate <ttencate@gmail.com>
2013-01-12 18:18:42 +0000

    Improve testing framework, xUnit style

--- test.sh
+++ test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 playnext=$(dirname $(readlink -f $0))/playnext
-media_dir=$(mktemp -d)
-media_dir_2=$(mktemp -d)
-progress_file=$(mktemp)
-
-args="-f $progress_file"
-if [[ $1 == "-v" ]]; then
-  args="$args -v"
-fi
 
 function fail() {
   echo "FAIL: $@"
@@ -20,6 +12,7 @@ function fail() {
 }
 
 function playnext() {
+  args="-f $progress_file"
   last_command="$playnext $args $@"
   $playnext $args "$@"
 }
@@ -44,85 +37,115 @@ function assert_fail() {
   ! playnext "$@" > /dev/null 2>&1 || fail "$message"
 }
 
-function reset_progress() {
-  rm $progress_file
+function set_up_fixture() {
+  media_dir_1=$(mktemp -d)
+  cd $media_dir_1
+  mkdir "Dir 1"
+  touch "Dir 1/File 1"
+  touch "Dir 1/file 2"
+  touch "Dir 1/File 3"
+  mkdir "Dir 2"
+  mkdir "Dir 3"
+  touch "Dir 3/File 1"
+  touch ".dotfile"
+  mkdir ".dotdir"
+  touch ".dotdir/file"
+
+  media_dir_2=$(mktemp -d)
+  cd $media_dir_2
+  mkdir "Dir 4"
+  touch "Dir 4/File 1"
+  touch "File 2"
+
+  media_dir_3=$(mktemp -d)
+
+  progress_file=$(mktemp)
 }
 
-cd $media_dir
+function tear_down_fixture() {
+  rm -r "$media_dir_1" "$media_dir_2" "$media_dir_3"
+}
 
-# Some files that should be ignored
-touch ".dotfile"
-mkdir ".dotdir"
-touch ".dotdir/file"
+function set_up() {
+  cd ${TMPDIR-/tmp}
+}
 
-# Test with empty dir
-assert_fail "Did not fail with empty dir"
+function tear_down() {
+  rm -f $progress_file
+}
 
-# Test with some files
-mkdir "Dir 1"
-touch "Dir 1/File 1"
-touch "Dir 1/file 2"
-touch "Dir 1/File 3"
-mkdir "Dir 2"
-mkdir "Dir 3"
-touch "Dir 3/File 1"
+function test_with_empty_dir() {
+  cd $media_dir_3
+  assert_fail "Did not fail with empty dir"
+}
 
-assert_output "$media_dir/Dir 1/File 1"
-assert_output "$media_dir/Dir 1/file 2"
-assert_output "$media_dir/Dir 1/File 3"
-assert_output "$media_dir/Dir 3/File 1"
-assert_fail "Did not run out of files"
+function test_with_some_files() {
+  cd $media_dir_1
+  assert_output "$media_dir_1/Dir 1/File 1"
+  assert_output "$media_dir_1/Dir 1/file 2"
+  assert_output "$media_dir_1/Dir 1/File 3"
+  assert_output "$media_dir_1/Dir 3/File 1"
+  assert_fail "Did not run out of files"
+}
 
-# Test multiple episodes in progress
-reset_progress
-playnext "Dir 1" -e "Dir 1/File 1" > /dev/null
-playnext "Dir 3" -e "Dir 3/File 1" > /dev/null
-assert_fail "Did not warn about multiple episodes in progress"
+function test_multiple_episodes_in_progress() {
+  cd $media_dir_1
+  playnext "Dir 1" -e "Dir 1/File 1" > /dev/null
+  playnext "Dir 3" -e "Dir 3/File 1" > /dev/null
+  assert_fail "Did not warn about multiple episodes in progress"
+}
 
-# Test progress recording for multiple media dirs
-reset_progress
-playnext > /dev/null
-pushd $media_dir_2 > /dev/null
-mkdir "Dir 4"
-touch "Dir 4/File 1"
-touch "File 2"
-playnext > /dev/null
-popd > /dev/null
-assert_output "$(echo -e "$media_dir/Dir 1/File 1\n$media_dir_2/Dir 4/File 1")" -l
+function test_multiple_media_dirs() {
+  cd $media_dir_1
+  playnext > /dev/null
+  cd $media_dir_2
+  playnext > /dev/null
+  assert_output "$(echo -e "$media_dir_1/Dir 1/File 1\n$media_dir_2/Dir 4/File 1")" -l
+}
 
-# Test directory argument
-reset_progress
-pushd /tmp > /dev/null
-assert_output "$media_dir/Dir 1/File 1" $media_dir
-assert_fail "Did not fail with multiple directories" $media_dir $media_dir_2
-popd > /dev/null
+function test_directory_argument() {
+  assert_output "$media_dir_1/Dir 1/File 1" $media_dir_1
+  assert_fail "Did not fail with multiple directories" $media_dir_1 $media_dir_2
+}
 
-# Test -e
-assert_output "$media_dir/Dir 1/file 2" -e "Dir 1/file 2"
-assert_output "$media_dir/Dir 1/File 3"
-assert_fail "Invalid file for -e was accepted" -e /dev/null
+function test_episode_option() {
+  cd $media_dir_1
+  assert_output "$media_dir_1/Dir 1/file 2" -e "Dir 1/file 2"
+  assert_output "$media_dir_1/Dir 1/File 3"
+  assert_fail "Invalid file for -e was accepted" -e /dev/null
+}
 
-# Test -h
-playnext -h | grep -q "Usage" || fail "Did not print usage"
+function test_help_option() {
+  playnext -h | grep -q "Usage" || fail "Did not print usage"
+}
 
-# Test -l
-reset_progress
-file=$(playnext)
-assert_output "$file" -l
+function test_list_option() {
+  playnext $media_dir_1 > /dev/null
+  playnext $media_dir_2 > /dev/null
+  file=$(cat $progress_file)
+  assert_output "$file" -l
+}
 
-# Test -n
-reset_progress
-assert_output "$media_dir/Dir 1/File 1"
-assert_output "$media_dir/Dir 1/file 2" -n
-assert_output "$media_dir/Dir 1/file 2" -n
+function test_no_advance_option() {
+  cd $media_dir_1
+  assert_output "$media_dir_1/Dir 1/File 1"
+  assert_output "$media_dir_1/Dir 1/file 2" -n
+  assert_output "$media_dir_1/Dir 1/file 2" -n
+}
 
-# Test -p
-reset_progress
-assert_output "$media_dir/Dir 1/File 1"
-assert_output "$media_dir/Dir 1/File 1" -p
-assert_output "$media_dir/Dir 1/File 1" -p
+function test_previous_option() {
+  cd $media_dir_1
+  assert_output "$media_dir_1/Dir 1/File 1"
+  assert_output "$media_dir_1/Dir 1/File 1" -p
+  assert_output "$media_dir_1/Dir 1/File 1" -p
+}
 
-rm -r "$media_dir"
-rm -r "$progress_file"
-
-echo "PASS"
+set_up_fixture
+trap tear_down_fixture EXIT
+while read function; do
+  [[ ! $function = test* ]] && continue
+  set_up
+  $function
+  tear_down
+  echo "PASS: $function"
+done < <(declare -F | cut -d' ' -f3)
